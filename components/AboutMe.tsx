04b86c7cd36be449b34c41cb558860f9d610f9db
Sean Slicer <seanslicer@gmail.com>
2022-09-12 00:41:19 -0500

    safari glow fix

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,4 +1,4 @@
-import Column from "../components/layout/Column";
+import { isSafari } from "react-device-detect";
 
 export const AboutMe = () => {
   return (
@@ -10,7 +10,11 @@ export const AboutMe = () => {
         <article id="AboutMeDescription">
           <div className="max-w-7xl mx-auto">
             <div className="relative group">
-              <div className="absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 lg:group-hover:opacity-100 transition duration-500 group-hover:duration-400 "></div>
+              <div
+                className={`absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 ${
+                  isSafari ? "" : "group-hover:opacity-100"
+                }}transition duration-500 group-hover:duration-400 `}
+              ></div>
               <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
                 <p className="my-4 text-4xl tracking-tight text-center text-gray-900 dark:text-white">
                   I&apos;m Sean. An efficient engineer with great detailing
