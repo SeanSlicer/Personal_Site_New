de78c6fb595e61880d25996e73eee56a3cf54912
Sean Slicer <seanslicer@gmail.com>
2022-09-12 01:06:47 -0500

    webkit is annoying / hover removed

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -1,5 +1,3 @@
-import { isSafari } from "react-device-detect";
-
 export const AboutMe = () => {
   return (
     <section id="AboutMe">
@@ -11,11 +9,8 @@ export const AboutMe = () => {
           <div className="max-w-7xl mx-auto">
             <div className="relative group">
               <div
-                className={`absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 ${
-                  isSafari
-                    ? " "
-                    : " group-hover:opacity-100 transition duration-500 group-hover:duration-400"
-                }`}
+                className={`absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25
+                `}
               ></div>
               <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
                 <p className="my-4 text-4xl tracking-tight text-center text-gray-900 dark:text-white">
