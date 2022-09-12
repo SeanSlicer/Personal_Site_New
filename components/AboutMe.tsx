425190daddec89fc3938ba7c4f97176671672356
Sean Slicer <seanslicer@gmail.com>
2022-09-12 01:04:10 -0500

    working on my machine hopeful fix

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -14,7 +14,7 @@ export const AboutMe = () => {
                 className={`absolute -inset-1 bg-gradient-to-r from-purple-600 to-pink-600 rounded-lg blur opacity-25 ${
                   isSafari
                     ? " "
-                    : " group-hover:opacity-100 transition duration-500 group-hover:duration-400 "
+                    : " group-hover:opacity-100 transition duration-500 group-hover:duration-400"
                 }`}
               ></div>
               <div className="relative px-7 py-6 bg-white ring-1 ring-gray-900/5 rounded-lg leading-none flex items-top justify-start space-x-6">
