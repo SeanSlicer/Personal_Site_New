c833b9e025fc5578b7f787eace3edad82ff14d8f
Sean Slicer <seanslicer@gmail.com>
2022-09-23 01:36:23 -0500

    title added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,19 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { ToastContainer, toast } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <div>
+      <Head>
+        <title>Sean Slicer Personal Site</title>
+        <meta
+          property="og:title"
+          content="Sean Slicer Personal Site"
+          key="title"
+        />
+      </Head>
       <ToastContainer
         position="top-right"
         autoClose={5000}
