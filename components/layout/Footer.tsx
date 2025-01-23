17cdb596481f4304ed12c31c4b90cfc4c46e94f0
Sean Slicer <SeanSlicer@gmail.com>
2025-01-22 21:57:20 -0600

    fix for error

--- components/layout/Footer.tsx
+++ components/layout/Footer.tsx
@@ -1,18 +1,19 @@
 import Color from "color";
 import Link from "next/link";
-import React from "react";
+import { FC, useState } from "react";
 
 type Props = {
   color?: string;
 };
 
-export const Footer: React.FC<Props> = (props) => {
+export const Footer: FC<Props> = (props) => {
+  const [date, setDate] = useState<Date>(new Date());
   const { color } = props;
 
   return (
     <footer className="shadow-sm-bottom border-t border-gray-200 bg-white">
       <div className="text-center p-6 bg-gray-200">
-        <span>© {new Date().getFullYear()} Copyright Sean Slicer</span>
+        <span>© {date.getFullYear()} Copyright Sean Slicer</span>
       </div>
     </footer>
   );
